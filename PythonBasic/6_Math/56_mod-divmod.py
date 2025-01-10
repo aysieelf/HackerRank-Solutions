2e96005af46a8e143a1ea861c7dd47538870b210
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-10 11:03:48 +0200

    solve 56_mod-divmod.py

---
+++ PythonBasic/6_Math/56_mod-divmod.py
@@ -0,0 +1,19 @@
+"""
+Task:
+    Read in two integers, a and b, and print three lines.
+    The first line is the integer division.
+    The second line is the result of the modulo operator: % .
+    The third line prints the divmod of a and b.
+
+Input Format:
+    The first line contains the first integer, a, and the second line contains the second integer, b.
+
+Output Format:
+    Print the result as described above.
+"""
+a = int(input())
+b = int(input())
+
+print(a//b)
+print(a%b)
+print(divmod(a, b))
